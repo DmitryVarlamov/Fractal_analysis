ef850b1aec8f2e49a3620fbc59494be31580d84a
DmitryVarlamov <varlamov_dmitry99@mail.ru>
2021-02-25 14:26:54 +0300

    Add results

--- Lab1/count_dimension.py
+++ Lab1/count_dimension.py
@@ -7,9 +7,11 @@ def count_dimension(image):
     e = {2: [0, 0, 0], 4: [0, 0, 0], 8: [0, 0, 0], 16: [0, 0, 0], 32: [0, 0, 0], 64: [0, 0, 0]}
     # Итоерируемся по ключам
     for e_i in e:
+        # Итерируемся по ячейкам
         for i in range(0, im.size[0]//e_i):
             for j in range(0, im.size[1]//e_i):
                 found_black = False
+                # Итерируемся по элементам ячейки
                 for k in range(i*e_i, ((i+1)*e_i) - 1):
                     for l in range(j*e_i, ((j+1)*e_i) - 1):
                         if image.getpixel((k,l)) == 0:
@@ -24,6 +26,7 @@ def count_dimension(image):
     ln_e_list = [e[x][2] for x in e]
     # Список значений ln(N(eps))
     ln_n_e_list = [e[x][1] for x in e]
+    # Вызов МНК
     return -np.polyfit(ln_e_list, ln_n_e_list, 1)[0]
 
 def convert_image(image, converted_file_name):
